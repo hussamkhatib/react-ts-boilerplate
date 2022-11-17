24320aba43f540323fc839e79a7e3dd91e13ce15
hussamkhatib <hussamkhatib@gmail.com>
2022-11-17 11:55:31 +0530

    -

---
+++ README.md
@@ -0,0 +1,9 @@
+### React + TS boilerplate
+
+Whats in it
+
+- Tailwind CSS
+  - icons -> heroicons
+  - a11y -> headless ui
+  - classNames
+- React-hot-toast
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Toaster } from "react-hot-toast";
+
 function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">
         React + Tailwind boilerplate
       </h1>
+      <Toaster />
     </div>
   );
 }
